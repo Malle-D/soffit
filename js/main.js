958d26f1b217aad718797af1038a275aa021336f
Malle-D <isfan250902@gmail.com>
2022-03-06 20:24:44 +0500

    Initial commit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var elHeader = document.querySelector(".header");
 let elBtn = document.querySelector(".header__button");
+let elBody = document.querySelector("body");
 
 elBtn.addEventListener('click', function(){
     elHeader.classList.toggle("header--opener");
+    elBody.classList.toggle("body--add");
 })
 
 
